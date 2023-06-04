e60e505b710b250b3f8b34e13cf32ba45a7d616d
Evan Nemetz <evannemetz@gmail.com>
2023-06-04 18:35:51 -0400

    Initial Commit

---
+++ Backlog/BacklogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BacklogApp.swift
+//  Backlog
+//
+//  Created by Evan Nemetz on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct BacklogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
